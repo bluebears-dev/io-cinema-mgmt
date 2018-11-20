60b1167854d04c3f3492d48b86f679d3c8d4ba46
Paweł Górski <dev.baymax42@gmail.com>
2018-11-21 00:17:47 +0100

    Changed * eslint formatting to indent at begining * minor style issues in ticket template

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
+    'plugin:vue/essential',
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
     'standard'
   ],
@@ -21,9 +21,20 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
+    'vue/script-indent': ['error', 2, {
+      'baseIndent': 1,
+    }],
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  }
+  },
+  overrides: [
+    {
+      'files': ['*.vue'],
+      'rules': {
+        'indent': 'off'
+      }
+    }
+  ]
 }
