69287861543a4ac1ebd1f199c1d24f9b2d57dad7
Paweł Górski <dev.baymax42@gmail.com>
2018-11-27 18:47:03 +0100

    * added simple cookie reading utility * added prototype to vue for cookie.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
 import Vuetify from 'vuetify'
+import '@mdi/font/css/materialdesignicons.css'
+
+import cookie from './utils/cookie'
+import App from './App'
 import router from './router'
 import store from './store'
-import '@mdi/font/css/materialdesignicons.css'
 
 Vue.use(Vuetify, {
   options: {
@@ -22,6 +24,7 @@ Vue.use(Vuetify, {
 })
 Vue.config.productionTip = false
 
+Vue.prototype.$cookie = cookie
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
