5971438a0789469ffd82b0df3ec92df1c63865e8
Paweł Górski <dev.baymax42@gmail.com>
2018-11-27 18:48:21 +0100

    * partially registered routes for movie and cinema * added global navigation guards * added validation in AppCinemaSelection and route changing

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,48 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import cookie from '../utils/cookie'
 
 Vue.use(Router)
 
-export default new Router({
+// ******************* Routes ******************* //
+
+const router = new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: null
+      path: '/kino',
+      name: 'CinemaSelection',
+      component: require('@/views/AppCinemaSelection.vue').default,
+      beforeEnter: (to, from, next) => {
+        let cinemaCookie = cookie.get('cinema')
+        if (cinemaCookie.value == null) {
+          next()
+        } else {
+          next({ name: 'Movies' })
+        }
+      }
+    },
+    {
+      path: '/repertuar',
+      name: 'Movies',
+      component: require('@/views/AppCinemaSelection.vue').default,
+      props: true
+    },
+    {
+      path: '*',
+      redirect: '/repertuar'
     }
   ]
 })
+
+// ********** Global Navigation Guards ********** //
+
+router.beforeEach((to, from, next) => {
+  let cinemaCookie = cookie.get('cinema')
+  if (cinemaCookie.value == null) {
+    next(false)
+  } else {
+    next()
+  }
+})
+
+export default router
