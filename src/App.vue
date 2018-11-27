78ac7fb938da314b25439f2698b1e57b60a348c8
Ada Sozańska <ada.sozanska@gmail.com>
2018-11-27 13:40:23 +0100

    * cinema selection form moved to separate component
    * fonts edited

--- src/App.vue
+++ src/App.vue
@@ -6,43 +6,7 @@
         fluid
         fill-height
       >
-        <v-layout
-          justify-center
-          align-center
-        >
-          <v-flex sm9 md7 lg5>
-            <form class="cinema--form">
-              <div class="algreya--regular form--text text-xs-center">Wybierz Kino</div>
-              <v-layout
-                justify-center
-                align-center
-                row
-                wrap
-              >
-                <v-flex sm10 md8 lg7>
-                  <v-select
-                    outline
-                    light
-                    label="Kino"
-                    color="gold"
-                    background-color="gold"
-                    class="gold--text"
-                    :items="['fajne', 'fajniejsze', 'jeszcze fajniejsze']"
-                  ></v-select>
-                </v-flex>
-                <v-flex xs12 class="text-xs-center button--padding">
-                  <v-btn
-                    class="algreya--regular text-capitalize"
-                    depressed
-                    large
-                    color="gold"
-                  >Dalej
-                  </v-btn>
-                </v-flex>
-              </v-layout>
-            </form>
-          </v-flex>
-        </v-layout>
+        <AppCinemaSelection/>
       </v-container>
     </v-content>
   </v-app>
@@ -50,10 +14,11 @@
 
 <script>
   import AppToolbar from './components/AppToolbar'
+  import AppCinemaSelection from './views/AppCinemaSelection'
 
   export default {
     name: 'App',
-    components: {AppToolbar}
+    components: { AppCinemaSelection, AppToolbar }
   }
 </script>
 
@@ -61,17 +26,4 @@
   @import '@mdi/font/css/materialdesignicons.min.css'
   @import 'vuetify/dist/vuetify.min.css'
   @import "assets/stylus/main.styl"
-
-  .cinema--form
-    background-color: var(--v-white-base)
-    padding: 60px 20px
-
-  .form--text
-    color: var(--v-charcoal-base)
-    font-size: 1.8rem
-    letter-spacing: 2px !important
-    padding-bottom: 30px
-
-  .button--padding
-    padding-top: 10px
 </style>
---
+++ src/views/AppCinemaSelection.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-layout
+    justify-center
+    align-center
+  >
+    <v-flex sm9 md7 lg5>
+      <form class="cinema--form">
+        <div class="alegreya-sc--regular form--text text-xs-center">Wybierz Kino</div>
+        <v-layout
+          justify-center
+          align-center
+          row
+          wrap
+        >
+          <v-flex sm10 md8 lg7>
+            <v-select
+              outline
+              light
+              label="Kino"
+              color="gold"
+              background-color="gold"
+              class="gold--text"
+              :items="['fajne', 'fajniejsze', 'jeszcze fajniejsze']"
+            ></v-select>
+          </v-flex>
+          <v-flex xs12 class="text-xs-center button--padding">
+            <v-btn
+              class="alegreya-sc--light text-capitalize button--font"
+              depressed
+              large
+              color="gold"
+            >Dalej
+            </v-btn>
+          </v-flex>
+        </v-layout>
+      </form>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+  export default {
+    name: 'AppCinemaSelection'
+  }
+</script>
+
+<style scoped lang="stylus">
+  .cinema--form
+    background-color: var(--v-white-base)
+    padding: 60px 20px
+
+  .form--text
+    color: var(--v-charcoal-base)
+    font-size: 1.8rem
+    letter-spacing: 2px !important
+    padding-bottom: 30px
+
+  .button--padding
+    padding-top: 10px
+
+  .button--font
+    font-size 1.6rem
+</style>
