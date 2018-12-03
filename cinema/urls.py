6c9b3a3ad0d5cc318f223a18cdc9bcee8f1a3ff0
Paweł Górski <dev.baymax42@gmail.com>
2018-12-03 15:13:20 +0100

    Created views and registered it to corresponding urls.

--- cinema/urls.py
+++ cinema/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
   path('', views.index, name='index'),
+  path('cinema/', views.CinemaListView.as_view()),
+  path('cinema/<str:key>/', views.CinemaDetailsView.as_view()),
 ]
--- cinema/views.py
+++ cinema/views.py
@@ -1,5 +1,31 @@
+from django.http import Http404
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from cinema.models import Cinema
+from cinema.serializers import CinemaSerializer, CinemaBriefSerializer
+
+
+class CinemaListView(APIView):
+  def get(self, request, format=None):
+    cinema = Cinema.objects.all()
+    serializer = CinemaBriefSerializer(cinema, many=True)
+    return Response(serializer.data)
+
+
+class CinemaDetailsView(APIView):
+  def _get(self, key):
+    try:
+      return Cinema.objects.get(pk=key)
+    except Cinema.DoesNotExist:
+      raise Http404
+
+  def get(self, request, key, format=None):
+    cinema = self._get(key)
+    serializer = CinemaSerializer(cinema)
+    return Response(serializer.data)
 
 
 def index(request):
-    return render(request, 'index.html')
+  return render(request, 'index.html')
