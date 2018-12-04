b446c979cbfa7cdda21678337f8a27eb70398924
Paweł Górski <dev.baymax42@gmail.com>
2018-12-04 13:13:06 +0100

    Auto-documentation for API

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
+from rest_framework.documentation import include_docs_urls
+from rest_framework.schemas import get_schema_view
 
 from . import views
 
 urlpatterns = [
   path('', views.index, name='index'),
   path('cinema/', views.CinemaListView.as_view()),
-  path('prices/', views.TicketTypeView.as_view())
+  path('prices/', views.TicketTypeView.as_view()),
+  path('docs/', include_docs_urls(title='KAPPA Cinema API')),
 ]
--- cinema/views.py
+++ cinema/views.py
@@ -1,4 +1,3 @@
-from django.http import Http404
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -9,6 +8,9 @@ from cinema.serializers import CinemaSerializer, TicketTypeSerializer
 
 class CinemaListView(APIView):
   def get(self, request, format=None):
+    """
+    Return a list of all cinemas in the system (with details)
+    """
     cinema = Cinema.objects.all()
     serializer = CinemaSerializer(cinema, many=True)
     return Response(serializer.data)
@@ -16,6 +18,9 @@ class CinemaListView(APIView):
 
 class TicketTypeView(APIView):
   def get(self, request, format=None):
+    """
+    Return a list of all cinemas in the system (with details)
+    """
     ticket_type = TicketType.objects.all()
     serializer = TicketTypeSerializer(ticket_type, many=True)
     return Response(serializer.data)
