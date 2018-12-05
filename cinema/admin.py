d5e95454f01c40b4189b1bd361331e09bc93cce2
Paweł Górski <dev.baymax42@gmail.com>
2018-12-05 23:58:31 +0100

    Redone Movie model, added filtering and display of custom fields (without genre)

--- cinema/admin.py
+++ cinema/admin.py
@@ -1,18 +1,29 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
-from .models import UserProfile
+from .models import UserProfile, Movie
 
 
 class UserInline(admin.StackedInline):
-  model = UserProfile
-  can_delete = False
+	model = UserProfile
+	can_delete = False
+
 
 class UserAdmin(BaseUserAdmin):
-  """
-  User and cinema Client model with user profile form
-  """
-  inlines = (UserInline,)
+	"""
+		User and cinema Client model with user profile form
+	"""
+	inlines = (UserInline,)
+
+
+class MovieAdmin(admin.ModelAdmin):
+	"""
+		Movie list with filtering
+	"""
+	list_display = ('title', 'releaseDate', 'producer', 'length')
+	list_filter = ('releaseDate', 'producer')
+
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
+admin.site.register(Movie, MovieAdmin)
--- cinema/models/movies.py
+++ cinema/models/movies.py
@@ -1,66 +1,58 @@
 from django.db import models
 from django.utils.translation import gettext as _
-from django.contrib import admin
 
 
-# todo ważne, jak już się będzie dało, zrób test, czy jak dodasz obraz i go usuniesz, to czy plik zostanie,
-#  jeśli tak, to nadpisz metodę delete, żeby najpierw usuwało obraz, a dopiero później obiekt
 class Movie(models.Model):
-    """One set of values represents one movie"""
+	"""
+		Model representing movie played at the cinema
+	"""
+	title = models.CharField(max_length=100, verbose_name=_('Tytuł'))
+	releaseDate = models.DateField(verbose_name=_('Data produkcji'))
+	length = models.IntegerField(verbose_name=_('Czas trwania'))
+	producer = models.CharField(max_length=50, verbose_name=_('Reżyseria'))
+	description = models.TextField(verbose_name=_('Opis'))
+	cover = models.ImageField(unique=True, max_length=200, verbose_name=_('Okładka'))
 
-    title = models.CharField(max_length=100, verbose_name=_('Tytuł'))
-    releaseDate = models.DateField(verbose_name=_('Data produkcji'))
-    length = models.IntegerField(verbose_name=_('Czas trwania'))
-    producer = models.CharField(max_length=50, verbose_name=_('Reżyseria'))
-    description = models.TextField(verbose_name=_('Opis'))
-    cover = models.ImageField(unique=True, max_length=200, verbose_name=_('Okładka'))
+	class Meta:
+		unique_together = (("title", "producer"),)
+		verbose_name = _('Film')
+		verbose_name_plural = _('Filmy')
 
-    class Meta:
-        unique_together = (("title", "producer"),)
-        verbose_name = _('Film')
-        verbose_name_plural = _('Filmy')
+	def __str__(self):
+		return _('{0}'.format(self.title))
 
-    def __str__(self):
-        return _('{0}'.format(self.title))
-
-
-# todo: zmienić MovieGenre jako pojedynczy gatunek
-class MovieGenre(models.Model):
-
-    # choices for genre, should be set normal when the time is right
-    G1 = 'gat1'
-    G2 = 'gat2'
-    HP = 'wiz'
-    FB = 'lotr'
-    HO = 'hor'
-    KO = 'kom'
-    LA = 'gat'
-    UPS = 'ext'
-
-    CHOICES_FOR_GENRE = (
-      (G1, 'gatunek pierwszy'),
-      (G2, 'gatunek drugi'),
-      (HP, "you're a wizard, Harry"),
-      (FB, 'you shall not pass'),
-      (HO, 'sikasz ze strachu'),
-      (KO, 'sikasz ze smiechu'),
-      (LA, 'ostatni egzemplarz gatunków'),
-      (UPS, 'extinction'),
-    )
-
-    # end choices
-
-    """One movie can have more than one movie_genre but they should not be repeated"""
-    movie = models.ForeignKey(Movie, models.CASCADE, verbose_name=_('Film'))
-    genre = models.CharField(max_length=20, choices=CHOICES_FOR_GENRE, verbose_name=_('Gatunek'))
-
-    class Meta:
-        verbose_name = _('Gatunek')
-        verbose_name_plural = _('Gatunki')
-
-    def __str__(self):
-        return _('{0} : {1}'.format(self.movie, self.genre))
-
-
-admin.site.register(Movie)
-admin.site.register(MovieGenre)
+#
+# # todo: zmienić MovieGenre jako pojedynczy gatunek
+# class MovieGenre(models.Model):
+# 	G1 = 'gat1'
+# 	G2 = 'gat2'
+# 	HP = 'wiz'
+# 	FB = 'lotr'
+# 	HO = 'hor'
+# 	KO = 'kom'
+# 	LA = 'gat'
+# 	UPS = 'ext'
+#
+# 	CHOICES_FOR_GENRE = (
+# 		(G1, 'gatunek pierwszy'),
+# 		(G2, 'gatunek drugi'),
+# 		(HP, "you're a wizard, Harry"),
+# 		(FB, 'you shall not pass'),
+# 		(HO, 'sikasz ze strachu'),
+# 		(KO, 'sikasz ze smiechu'),
+# 		(LA, 'ostatni egzemplarz gatunków'),
+# 		(UPS, 'extinction'),
+# 	)
+#
+# 	# end choices
+#
+# 	"""One movie can have more than one movie_genre but they should not be repeated"""
+# 	movie = models.ForeignKey(Movie, models.CASCADE, verbose_name=_('Film'))
+# 	genre = models.CharField(max_length=20, choices=CHOICES_FOR_GENRE, verbose_name=_('Gatunek'))
+#
+# 	class Meta:
+# 		verbose_name = _('Gatunek')
+# 		verbose_name_plural = _('Gatunki')
+#
+# 	def __str__(self):
+# 		return _('{0} : {1}'.format(self.movie, self.genre))
