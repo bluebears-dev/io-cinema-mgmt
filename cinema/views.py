7997baaaae28d50ee2c0b68a6c1565a2aaf158d9
Paweł Górski <dev.baymax42@gmail.com>
2018-12-07 13:41:34 +0100

    Indentation problem fixed

--- cinema/views.py
+++ cinema/views.py
@@ -7,24 +7,24 @@ from cinema.serializers import CinemaSerializer, TicketTypeSerializer
 
 
 class CinemaListView(APIView):
-	def get(self, request, format=None):
-		"""
-			Return a list of all cinemas in the system (with details)
-		"""
-		cinema = Cinema.objects.all()
-		serializer = CinemaSerializer(cinema, many=True)
-		return Response(serializer.data)
+    def get(self, request, format=None):
+        """
+            Return a list of all cinemas in the system (with details)
+        """
+        cinema = Cinema.objects.all()
+        serializer = CinemaSerializer(cinema, many=True)
+        return Response(serializer.data)
 
 
 class TicketTypeView(APIView):
-	def get(self, request, format=None):
-		"""
-			Return a list of all cinemas in the system (with details)
-		"""
-		ticket_type = TicketType.objects.all()
-		serializer = TicketTypeSerializer(ticket_type, many=True)
-		return Response(serializer.data)
+    def get(self, request, format=None):
+        """
+            Return a list of all cinemas in the system (with details)
+        """
+        ticket_type = TicketType.objects.all()
+        serializer = TicketTypeSerializer(ticket_type, many=True)
+        return Response(serializer.data)
 
 
 def index(request):
-	return render(request, 'index.html')
+    return render(request, 'index.html')
