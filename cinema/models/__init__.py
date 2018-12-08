9038b02aca7c2a2509b6360666c1a7a1e3b538c1
Przemek <sciemniak000@gmail.com>
2018-12-08 15:07:41 +0100

    Base is done
    Some things could be done better though so we shouldn't forget about it if we have any time before the final deadline.

--- cinema/models/__init__.py
+++ cinema/models/__init__.py
@@ -1,4 +1,4 @@
 from .user import UserProfile
 from .cinema import Cinema, Room, Seat, Showing
 from .booking import TicketType, Ticket, Booking
-from .movies import Movie
+from .movies import Movie, MovieGenre
