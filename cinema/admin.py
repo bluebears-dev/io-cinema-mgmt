1783238bd9d31ab7d8cb048d26c2714bb143c29e
Paweł Górski <dev.baymax42@gmail.com>
2018-12-09 01:01:01 +0100

    Fixes #61 #62

--- cinema/admin.py
+++ cinema/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
+from django.utils.translation import gettext as _
 
 from cinema.models import Cinema, Room, Booking
 from .models import EmployeeProfile, ClientProfile, Movie, TicketType, Showing, Client
@@ -28,12 +29,39 @@ class EmployeeAdmin(BaseUserAdmin):
         User and cinema Client model with user profile form
     """
     inlines = (EmployeeInline,)
+    GROUP_NAMES = {
+        'EDITORS': 'Redaktorzy',
+        'DIRECTORS': 'Kierownictwo',
+        'ADMINS': 'Administratorzy'
+    }
 
-    def formfield_for_dbfield(self, db_field, **kwargs):
-        field = super(EmployeeAdmin, self).formfield_for_dbfield(db_field, **kwargs)
-        if db_field.name == 'is_staff':
-            field.initial = True
-        return field
+    def save_model(self, request, obj, form, change):
+        """
+            Save model depending on user group and set default value
+        :param request:
+        :param obj:
+        :param form:
+        :param change:
+        :return:
+        """
+        obj.is_staff = True
+        super().save_model(request, obj, form, change)
+
+    def get_fieldsets(self, request, obj=None):
+        """
+            Restrict fieldsets depending on user group2
+        :param request:
+        :param obj:
+        :return:
+        """
+        fieldsets = super(EmployeeAdmin, self).get_fieldsets(request, obj)
+        current_user = request.user
+        user_groups = [v.name for v in current_user.groups.all()]
+        if EmployeeAdmin.GROUP_NAMES['ADMINS'] not in user_groups:
+            fieldsets = filter(lambda fieldset: fieldset[0] != _('Permissions'), fieldsets)
+        fieldsets = filter(lambda fieldset: fieldset[0] != _('Important dates'), fieldsets)
+        return fieldsets
+
 
     def get_queryset(self, request):
         """
