860beb58eb026e448c0e0d8a989e08e28b007496
Paweł Górski <dev.baymax42@gmail.com>
2018-12-16 15:57:01 +0100

    Added transitions on movie list - changed v-layout for v-transition

--- src/components/AppMovie/MovieList.vue
+++ src/components/AppMovie/MovieList.vue
@@ -1,35 +1,40 @@
 <template>
-    <v-layout
-            row wrap
-            align-start
-            justify-center
+  <v-slide-x-transition
+      class="layout row wrap align-start justify-center"
+      group
+      hide-on-leave
+  >
+    <v-flex
+        :key="movie.id"
+        class="card--style text-xs-center"
+        v-for="movie in movies()"
     >
-        <v-flex v-for="movie in movies()" :key="movie.title" class="card--style text-xs-center">
-            <v-card
-                    color="black"
-                    flat
-                    class="mx-auto"
-                    width="240px"
-            >
-              <router-link :to="{name: 'MovieDetails', params: {id: movie.id}}" class="router--link">
-                    <v-img
-                            :src="movie.cover"
-                            class="image--style"
-                            height="340px"
-                    ></v-img>
-                    <div class="alegreya-sc--light movie--title">
-                        {{movie.title}}
-                    </div>
-                </router-link>
-            </v-card>
-        </v-flex>
-        <v-flex
-                v-if="!movies().length"
-                class="alegreya-sc--light no--showings text-xs-center"
-        >
-            Brak seansów na wybrany dzień.
-        </v-flex>
-    </v-layout>
+      <v-card
+          class="mx-auto"
+          color="black"
+          flat
+          width="240px"
+      >
+        <router-link :to="{name: 'MovieDetails', params: {id: movie.id}}" class="router--link">
+          <v-img
+              :src="movie.cover"
+              class="image--style"
+              height="340px"
+          ></v-img>
+          <div class="alegreya-sc--light movie--title">
+            {{movie.title}}
+          </div>
+        </router-link>
+      </v-card>
+    </v-flex>
+    <v-flex
+        class="alegreya-sc--light no--showings text-xs-center"
+        key="none"
+        v-if="!movies().length"
+    >
+      Brak seansów na wybrany dzień.
+    </v-flex>
+  </v-slide-x-transition>
 </template>
 
 <script>
@@ -59,27 +64,27 @@
 </script>
 
 <style scoped lang="stylus">
-    .image--style
-        width: 240px
-        border: 1.6px solid var(--v-gold-base)
+  .image--style
+    width: 240px
+    border: 1.6px solid var(--v-gold-base)
 
-    .card--style
-        padding-bottom: 20px
-        flex: 0 0 280px
+  .card--style
+    padding-bottom: 20px
+    flex: 0 0 280px
 
-    .movie--title
-        padding-top 7px
-        padding-bottom 10px
-        color: var(--v-gold-base)
-        letter-spacing: 2px
-        font-size: 1.5rem
-        hyphens: auto
+  .movie--title
+    padding-top 7px
+    padding-bottom 10px
+    color: var(--v-gold-base)
+    letter-spacing: 2px
+    font-size: 1.5rem
+    hyphens: auto
 
-    .router--link
-        text-decoration none
+  .router--link
+    text-decoration none
 
-    .no--showings
-        color: var(--v-white-base)
-        font-size: 2rem
-        padding-top: 100px
+  .no--showings
+    color: var(--v-white-base)
+    font-size: 2rem
+    padding-top: 100px
 </style>
