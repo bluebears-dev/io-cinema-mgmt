a21394679ec8ebefe956f1fcea5b3554fd3d4827
Przemek <sciemniak000@gmail.com>
2018-12-20 00:02:02 +0100

    Thumbnails tests created
    Cinema names tested too

--- cinema/tests/models/all_other_tests/test_movie_models.py
+++ cinema/tests/models/all_other_tests/test_movie_models.py
@@ -1,5 +1,5 @@
 from cinema.tests.models.base_for_tests import CinemaModelTest, NoTestOccuredException, Movie,\
-  File, BASE_DIR, check_adding_model_instance_with_wrong_fields, MovieGenre
+  File, BASE_DIR, check_adding_model_instance_with_wrong_fields, MovieGenre, MEDIA_ROOT
 
 
 class MovieTest(CinemaModelTest):
@@ -13,7 +13,6 @@ class MovieTest(CinemaModelTest):
     Movie.objects.create(title='Black clover', releaseDate='2011-02-22', length=1200, producer="Przemysław Indyka",
                          description="5 leave clover and great mystery of the future",
                          cover=File(open(BASE_DIR + '/test/static/wspawielkanocna.jpg', 'rb')))
-    picturee = Movie.objects.get(title="Black clover").cover
 
     # try to add a movie with a duplicate name
     check_adding_model_instance_with_wrong_fields(Movie, "Exception should occur but it hadn't when creating movie "
@@ -63,18 +62,6 @@ class MovieTest(CinemaModelTest):
                                                   description=None,
                                                   cover=File(open(BASE_DIR + '/test/static/wspawielkanocna.jpg', 'rb')))
 
-    # delete created movie and check if the cover file was deleted
-    Movie.objects.get(title="Black clover").delete()
-    try:
-      f = File(open(picturee))
-      raise NoTestOccuredException()
-    except NoTestOccuredException as e:
-      print("The cover file was not deleted after deletion of a movie model")
-      raise e
-    except Exception as ee:
-      # print("Expected exception info: " + e.__str__())
-      pass
-
   def test_movie_genre(self):
     # try to add a movie genre without a name
     check_adding_model_instance_with_wrong_fields(MovieGenre, "Exception should have occured but it hadn't when"
@@ -83,3 +70,34 @@ class MovieTest(CinemaModelTest):
     check_adding_model_instance_with_wrong_fields(MovieGenre, "Exception should have occured but it hadn't when"
                                                               " creating movie genre with too long name",
                                                   name="paraliz"*20)
+
+  def test_movie_files_management(self):
+    # add a movie
+    Movie.objects.create(title='Black clover', releaseDate='2011-02-22', length=1200, producer="Przemysław Indyka",
+                         description="5 leave clover and great mystery of the future",
+                         cover=File(open(BASE_DIR + '/test/static/wspawielkanocna.jpg', 'rb')))
+    picturee = Movie.objects.get(title="Black clover").cover.__str__()
+    thumbee = 'thumbs/' + picturee[picturee.find('/')+1:]
+
+    # delete created movie and check if the cover and thumbnail files were deleted
+    Movie.objects.get(title="Black clover").delete()
+    try:
+      f = File(open(MEDIA_ROOT + '/' + picturee))
+      raise NoTestOccuredException()
+    except NoTestOccuredException as e:
+      print("The cover file was not deleted after deletion of a movie model")
+      raise e
+    except Exception as ee:
+      # print("Expected exception info: " + ee.__str__())
+      pass
+
+    try:
+      f = File(open(MEDIA_ROOT + '/' + thumbee))
+      raise NoTestOccuredException()
+    except NoTestOccuredException as e:
+      print("The cover file was not deleted after deletion of a movie model")
+      raise e
+    except Exception as ee:
+      # print("Expected exception info: " + ee.__str__())
+      pass
+
--- cinema/tests/models/to_be_run_customly/test_cinema_model.py
+++ cinema/tests/models/to_be_run_customly/test_cinema_model.py
@@ -5,5 +5,5 @@ from cinema.tests.models.base_for_tests import CinemaModelTest, NoTestOccuredExc
 class MovieTest(CinemaModelTest):
 
   def test_str_representation(self):
-    self.assertEqual(self.movie_wizard.__str__(), "Harry Potter")
-    self.assertEqual(self.anime.__str__(), "Anime")
+    self.assertEqual(self.cinema_lato.__str__(), "Lato")
+    self.assertEqual(self.showing_wizard1.__str__(), "Harry Potter (2018-12-23 12:54:00)")
