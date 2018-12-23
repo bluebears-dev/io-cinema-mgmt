7711180ac9ebf26ff510cdbf74f51b9f3f215a1b
Paweł Górski <dev.baymax42@gmail.com>
2018-12-24 00:29:28 +0100

    Documented and added initial values fetched from model.

--- cinema/forms.py
+++ cinema/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib import admin
+from django.utils.translation import gettext as _
 
 from cinema.fields import LayoutField
 from .models import Showing, Room, EmployeeProfile, ClientProfile
@@ -38,10 +39,23 @@ class ShowingForm(forms.ModelForm):
 
 
 class RoomForm(forms.ModelForm):
-    rows = forms.IntegerField(min_value=1, max_value=70)
-    cols = forms.IntegerField(min_value=1, max_value=70)
-    layout = LayoutField()
+    """
+        Room form definition
+    """
+    rows = forms.IntegerField(label=_('Ilość rzędów'), min_value=1, max_value=70, initial=1)
+    cols = forms.IntegerField(label=_('Ilość kolumn'), min_value=1, max_value=70, initial=1)
+    layout = LayoutField(label=_('Rozkład sali'))
+
+    def __init__(self, *args, **kwargs):
+        instance = kwargs.get('instance')
+        super(RoomForm, self).__init__(*args, **kwargs)
+        # if editing load instance values
+        if instance:
+            self.fields['rows'].initial = instance.rows
+            self.fields['cols'].initial = instance.cols
+            self.fields['layout'].initial = instance.raw_layout
+            print(instance.raw_layout)
 
     class Meta:
         model = Room
-        fields = "__all__"
+        fields = ('name', 'cinema', 'rows', 'cols', 'layout',)
