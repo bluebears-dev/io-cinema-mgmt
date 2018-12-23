ddc147b1bcecc2f89381a215c4ac0a3728c10232
Paweł Górski <dev.baymax42@gmail.com>
2018-12-24 00:28:38 +0100

    Documented code and fixed some mistakes with value passing after failed validation.
    Fixed calculation of flat indexes.

--- cinema/widgets.py
+++ cinema/widgets.py
@@ -8,7 +8,8 @@ from app.settings import BASE_DIR
 
 class LayoutWidget(Input):
     """
-    Base class for all <input> widgets.
+        Layout widget class.
+        It dynamically creates layout form based on form values of rows and cols.
     """
     input_type = 'checkbox'
     template_name = os.path.join(BASE_DIR, 'cinema', 'templates', 'widgets', 'layout_widget.html')
@@ -18,14 +19,24 @@ class LayoutWidget(Input):
         self.format = format or None
 
     def get_context(self, name, value, attrs):
+        """
+            Sets context with keys like type of value to template
+        """
         context = super().get_context(name, value, attrs)
         context['widget']['type'] = self.input_type
         return context
 
     def format_value(self, value):
-        if value is None:
-            return json.dumps(value)
-        return json.dumps(value[0])
+        """
+            Return value to template.
+            Checks if its value after failed validation of from model.
+        """
+        if len(value) and isinstance(value[0], list):
+            return value[0]
+        return value
 
     def value_from_datadict(self, data, files, name):
+        """
+            Return tuple from widget template: layout as list, rows and cols.
+        """
         return data.getlist(name), data.get('rows'), data.get('cols')
