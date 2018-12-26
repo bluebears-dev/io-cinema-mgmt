d2a1a2bd64a76e371822d81bdc5db9e313551a97
Paweł Górski <dev.baymax42@gmail.com>
2018-12-26 18:58:55 +0100

    Created one utility script for running project and tests.

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-eval $(docker-machine env)
-docker-compose exec django coverage run ./manage.py test
-docker-compose exec django coverage xml
-
-echo "You can run now 'sonar-scanner -D sonar.host.url=http://`docker-machine ip`:9000/'"
