b490b3a21dff7e70a2a42101d3cdb63f6d897629
Paweł Górski <dev.baymax42@gmail.com>
2018-12-27 12:44:52 +0100

    Merge remote-tracking branch 'origin/heroku' into heroku

--- app/settings.py
+++ app/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -149,3 +151,5 @@ WEBPACK_LOADER = {
         'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.json'),
     }
 }
+
+django_heroku.settings(locals())
