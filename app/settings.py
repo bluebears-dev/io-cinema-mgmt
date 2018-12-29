682193f20e5712b0505cef047d1d12706bc50609
Paweł Górski <dev.baymax42@gmail.com>
2018-12-29 12:54:41 +0100

    Fixed issue with static files when gunicorn was not ran.

--- app/settings.py
+++ app/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'webpack_loader',
     'rest_framework'
--- app/wsgi.py
+++ app/wsgi.py
@@ -12,6 +12,8 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
+from app.settings import BASE_DIR
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 application = get_wsgi_application()
-application = WhiteNoise(application, root='/path/to/static/files')
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'static'))
