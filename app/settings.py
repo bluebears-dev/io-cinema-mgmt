2716a1ee656f20d2bcc602b0f4b437486960fccb
Paweł Górski <dev.baymax42@gmail.com>
2018-12-29 13:58:21 +0100

    Fixed naming issue when getting keys for AWS connection.

--- app/settings.py
+++ app/settings.py
@@ -79,8 +79,8 @@ TEMPLATES = [
 
 AWS_STORAGE_BUCKET_NAME = 'io-cinema-mgmt'
 AWS_S3_REGION_NAME = 'us-east-1'
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 WSGI_APPLICATION = 'app.wsgi.application'
