7d5cf0e2778feb77c7c68609ee0bc76966abc86b
Paweł Górski <dev.baymax42@gmail.com>
2018-12-29 14:12:29 +0100

    Changed region setting and added session token.

--- app/settings.py
+++ app/settings.py
@@ -78,9 +78,10 @@ TEMPLATES = [
 ]
 
 AWS_STORAGE_BUCKET_NAME = 'io-cinema-mgmt'
-AWS_S3_REGION_NAME = 'us-east-1'
+AWS_DEFAULT_REGION = 'us-east-1'
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_SESSION_TOKEN = os.environ.get('AWS_SESSION_TOKEN')
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 WSGI_APPLICATION = 'app.wsgi.application'
