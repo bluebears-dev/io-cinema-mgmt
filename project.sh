8a59ef4d6115b887396761db1720a1b53e661c88
Paweł Górski <dev.baymax42@gmail.com>
2018-12-29 12:54:56 +0100

    Refactored script.

--- project.sh
+++ project.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
+docker-machine start
+eval $(docker-machine env)
 case $1 in
     "build")
-        docker-machine start
-        eval $(docker-machine env)
         docker-compose down
         docker-compose build
     ;;
     "test")
-        eval $(docker-machine env)
         docker-compose exec django true
         if [[ $? -ne 0 ]]
         then
@@ -18,23 +17,15 @@ case $1 in
         fi
     ;;
     "run")
-        docker-machine start
-        eval $(docker-machine env)
         docker-compose up postgres django
     ;;
     "rund")
-        docker-machine start
-        eval $(docker-machine env)
         docker-compose up -d postgres django
     ;;
     "sonar")
-        docker-machine start
-        eval $(docker-machine env)
         docker-compose up -d sonarqube sonardata
     ;;
     "all")
-        docker-machine start
-        eval $(docker-machine env)
         docker-compose up -d sonarqube sonardata
         docker-compose up postgres django
     ;;
