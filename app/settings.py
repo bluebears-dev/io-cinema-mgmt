e9a8608d058cca7c9b5080ab75835bbbe1a0fff1
Paweł Górski <dev.baymax42@gmail.com>
2018-12-29 13:06:25 +0100

    Fixed major issues.
    File requirements.txt had corrupted data.
    Added starting docker for 'build' argument in script.
    Removed gunicorn from docker-compose because gunicorn prevented Django code hot-reloading.

--- app/settings.py
+++ app/settings.py
@@ -77,6 +77,12 @@ TEMPLATES = [
     },
 ]
 
+AWS_STORAGE_BUCKET_NAME = 'io-cinema-mgmt'
+AWS_S3_REGION_NAME = 'us-east-1'
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 WSGI_APPLICATION = 'app.wsgi.application'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace('\\', '/')
