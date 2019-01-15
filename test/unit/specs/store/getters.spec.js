1466c16c59996461aa7fd8b7d75d75d2c2c14286
Ada Sozańska <ada.sozanska@gmail.com>
2019-01-15 19:38:40 +0100

    tests for getters added

--- test/unit/specs/store/getters.spec.js
+++ test/unit/specs/store/getters.spec.js
@@ -1,10 +1,67 @@
 import getters from '@/store/getters'
+import state from '@/store/state'
 
 describe('getters.js', () => {
   it('getCurrentCinema should return correct value', () => {
     const state = {currentCinema: 'cinema'}
-    getters.getCurrentCinema(state)
+    const result = getters.getCurrentCinema(state)
 
-    expect(state.currentCinema).to.equal('cinema')
+    expect(result).to.equal('cinema')
+  })
+
+  it('getMovies should return correct value', () => {
+    const state = { movies: ['movie 1', 'movie 2'] }
+    const result = getters.getMovies(state)
+
+    expect(result).to.be.an('array')
+    expect(result).to.have.members(['movie 1', 'movie 2'])
+  })
+
+  it('getTicketTypes should return correct value', () => {
+    const state = { ticketTypes: ['type 1', 'type 2', 'type 3'] }
+    const result = getters.getTicketTypes(state)
+
+    expect(result).to.be.an('array')
+    expect(result).to.have.members(['type 1', 'type 2', 'type 3'])
+  })
+
+  it('getCinemas should return correct value', () => {
+    const state = { cinemas: ['cinema 1', 'cinema 2'] }
+    const result = getters.getCinemas(state)
+
+    expect(result).to.be.an('array')
+    expect(result).to.have.members(['cinema 1', 'cinema 2'])
+  })
+
+  it('getShowings should return correct value', () => {
+    const state = { showings: ['showing 1', 'showing 2'] }
+    const result = getters.getShowings(state)
+
+    expect(result).to.be.an('array')
+    expect(result).to.have.members(['showing 1', 'showing 2'])
+  })
+
+  it('getMovieDetails should return correct value', () => {
+    const state = { movieDetails: ['detail 1', 'detail 2'] }
+    const result = getters.getMovieDetails(state)
+
+    expect(result).to.be.an('array')
+    expect(result).to.have.members(['detail 1', 'detail 2'])
+  })
+
+  it('getCinemaDetails should return correct value', () => {
+    const state = { currentCinema: 2, cinemas: [{ id: 1, name: 'cinema 1' }, { id: 2, name: 'cinema 2' }] }
+    const result = getters.getCinemaDetails(state)
+
+    expect(result.name).to.be.equal('cinema 2')
+    expect(result.id).to.be.equal(2)
+  })
+
+  it('getSelectedDate  should return correct value', () => {
+    const result = getters.getSelectedDate(state)
+    const weekDays = ['Nd', 'Pn', 'Wt', 'Śr', 'Cz', 'Pt', 'So']
+
+    expect(result.title).to.be.oneOf(weekDays)
+    expect(result.date).to.match(/\d{4}-\d{2}-\d{2}/)
   })
 })
